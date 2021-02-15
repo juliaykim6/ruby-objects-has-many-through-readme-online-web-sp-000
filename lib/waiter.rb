3fd0062eb8069a072560d948dad8c3541f1523d6
Julia Kim <juliaykim6@gmail.com>
2021-02-15 02:45:27 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -19,8 +19,10 @@ class Waiter
   end
 
   def meals
-    Meal.all.select do {|meal| meal.waiter == self}
+    Meal.all.select do |meal|
+      meal.waiter == self
   end
+end
 
   def best_tipper
     best_tipped_meal = meals.max do |meal_a, meal_b|
